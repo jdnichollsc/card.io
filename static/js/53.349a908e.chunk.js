f38466dfd922c888a27ffe995050f81225dc29df
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2021-04-03 16:23:25 -0500

    Updates

---
+++ static/js/53.349a908e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcardio=this.webpackJsonpcardio||[]).push([[53],{177:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var i=e(16),o=e(12),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(n){return Object(o.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.349a908e.chunk.js.map
